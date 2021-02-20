861c09d7ea75e6e6cb7f688f32e38ab16c11a5f0
webnickell <50118217+webnickell@users.noreply.github.com>
2021-02-20 10:34:49 +0200

    change project structure

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:int20h_2020/ui/pages/home_page.dart';
+
+class Int20hApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
